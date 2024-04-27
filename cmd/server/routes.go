cf2bfd8c5cc76e1f4835e1958e7e2275b3a6c4db
Austin Townsend <Austintownsend1234@gmail.com>
2024-04-26 19:05:29 -0700

    FEATURE: Added routes func and middleware

--- cmd/server/routes.go
+++ cmd/server/routes.go
@@ -1,1 +1,21 @@
 package main
+
+import (
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/httplog/v2"
+	"net/http"
+)
+
+func (app *application) routes() http.Handler {
+	r := chi.NewRouter()
+
+	r.Use(middleware.RealIP)
+	r.Use(middleware.RequestID)
+	r.Use(httplog.RequestLogger(app.logger))
+	r.Use(middleware.Heartbeat("/ping"))
+	r.Use(middleware.CleanPath)
+	r.Use(middleware.Recoverer)
+
+	return r
+}
